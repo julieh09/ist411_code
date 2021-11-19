d737c67167e97af55afdb9803ca75d4cb540544e
NucciTheBoss <jcn23@psu.edu>
2021-11-19 02:12:58 -0500

    Submit working application

--- laptop_rental/client/client.js
+++ laptop_rental/client/client.js
@@ -53,7 +53,131 @@ function getCurrentUser() {
     currentUser = xhttpGetCurrentUser.responseText;
 }
 
+function createReservation() {
+    const xhttpCreateReservation = new XMLHttpRequest();
+
+    laptop = document.getElementById("laptops");
+    date = document.getElementById("date");
+    time = document.getElementById("time");
+    numHours = document.getElementById("numHours");
+
+    xhttpCreateReservation.open("POST", `http://127.0.0.1:5000/createReservation/${laptop.value}/${date.value}/${time.value}/${numHours.value}`);
+    xhttpCreateReservation.send();
+
+    xhttpCreateReservation.onload = function() {
+        console.log(this.responseText);
+    }
+}
+
+function updateReservation() {
+    const xhttpUpdateReservation = new XMLHttpRequest();
+
+    laptop = document.getElementById("laptops");
+    date = document.getElementById("date");
+    time = document.getElementById("time");
+    numHours = document.getElementById("numHours");
+
+    xhttpUpdateReservation.open("POST", `http://127.0.0.1:5000/updateReservation/${laptop.value}/${date.value}/${time.value}/${numHours.value}`);
+    xhttpUpdateReservation.send();
+
+    xhttpUpdateReservation.onload = function() {
+        console.log(this.responseText);
+    }
+}
+
+function deleteReservation() {
+    const xhttpDeleteReservation = new XMLHttpRequest();
+
+    xhttpDeleteReservation.open("DELETE", "http://127.0.0.1:5000/deleteReservation");
+    xhttpDeleteReservation.send();
+
+    xhttpDeleteReservation.onload = function() {
+        console.log(this.responseText);
+    }
+
+}
+
+function viewReservation() {
+    const xhttpViewReservation = new XMLHttpRequest();
+
+    xhttpViewReservation.open("GET", "http://127.0.0.1:5000/viewReservation");
+    xhttpViewReservation.send();
+
+    xhttpViewReservation.onload = function() {
+        // Present date information to user by displaying as a modal
+        let dataArray = [];
+        dataArray.push(JSON.parse(this.responseText));
+        let newTable = tableBuilder(dataArray, ["Username", "Laptop", "Start Date", "Start Time", "# of Hours"]);
+        document.querySelector("#resultHeader").innerHTML = `${currentUser}'s Reservation`;
+        document.querySelector("#resultBody").innerHTML = "";
+        document.querySelector("#resultBody").appendChild(newTable);
+    }
+}
+
+function viewAllReservations() {
+    const xhttpViewAllReservations = new XMLHttpRequest();
+
+    xhttpViewAllReservations.open("GET", "http://127.0.0.1:5000/viewAllReservations");
+    xhttpViewAllReservations.send();
+
+    xhttpViewAllReservations.onload = function() {
+        resz = JSON.parse(this.responseText);
+        let newTable = tableBuilder(resz.data, ["Username", "Laptop", "Start Date", "Start Time", "# of Hours"]);
+        document.querySelector("#resultHeader").innerHTML = "All Reservations";
+        document.querySelector("#resultBody").innerHTML = "";
+        document.querySelector("#resultBody").appendChild(newTable);
+    }
+}
+
 // Function to modify the current view
 function swapPage(pageName) {
     location.href = pageName;
+}
+
+function tableBuilder(tableData, tableHeaders) {
+    // Create root node of table
+    let tableRoot = document.createElement('table');
+    tableRoot.setAttribute("class", "table");
+
+    // Create table header
+    let tableHeader = document.createElement('thead');
+    let tableHeaderRow = document.createElement('tr');
+    tableHeaders.forEach((elem) => {
+        let tableH = document.createElement('th');
+        let tableHText = document.createTextNode(elem);
+        tableH.appendChild(tableHText);
+        tableHeaderRow.appendChild(tableH);
+    });
+    tableHeader.appendChild(tableHeaderRow);
+    tableRoot.appendChild(tableHeader);
+
+    // Create table body
+    let tableBody = document.createElement('tbody');
+    tableData.forEach((elem) => {
+        let tableBodyRow = document.createElement('tr');
+        let name = document.createElement('td');
+        let nameText = document.createTextNode(elem.username);
+        name.appendChild(nameText);
+        let laptop = document.createElement('td');
+        let laptopText = document.createTextNode(elem.laptop);
+        laptop.appendChild(laptopText);
+        let startDate = document.createElement('td');
+        let startDateText = document.createTextNode(elem.start_date);
+        startDate.appendChild(startDateText);
+        let startTime = document.createElement('td');
+        let startTimeText = document.createTextNode(elem.start_time);
+        startTime.appendChild(startTimeText);
+        let numHours = document.createElement('td');
+        let numHoursText = document.createTextNode(elem.num_hours);
+        numHours.appendChild(numHoursText);
+        tableBodyRow.appendChild(name);
+        tableBodyRow.appendChild(laptop);
+        tableBodyRow.appendChild(startDate);
+        tableBodyRow.appendChild(startTime);
+        tableBodyRow.appendChild(numHours);
+        tableBody.appendChild(tableBodyRow);
+    });
+    tableRoot.appendChild(tableBody);
+
+    return tableRoot;
 }
--- laptop_rental/server/app.js
+++ laptop_rental/server/app.js
@@ -93,28 +93,101 @@ app.get("/currentuser", (req, res) => {
 });
 
 // Create reservation
-app.post("/createReservation/:laptop/:date/:time/:hoursMax", (req, res) => {
+app.post("/createReservation/:laptop/:date/:time/:numHours", (req, res) => {
+    console.log(`Received request for reservation from ${currentUser}.`);
+    
+    // Array object to store reservation data
+    let resz = {
+        "username": currentUser,
+        "laptop": req.params.laptop,
+        "start_date": req.params.date,
+        "start_time": req.params.time,
+        "num_hours": req.params.numHours
+    }
 
+    // Check if the user already has a reservation
+    const userIndex = data.resList.findIndex((elem) => {elem.username == currentUser});
+
+    if (userIndex == -1) {
+        data.resList.push(resz);
+        dumpFlatFile(data);
+        res.send("200 OK.");
+
+    } else {
+        res.send("504 - USER ALREADY EXISTS.");
+    }
 });
 
 // Update reservation
 app.post("/updateReservation/:newLaptop/:newDate/:newTime/:newHoursMax", (req, res) => {
+    console.log(`Received request to update reservation from ${currentUser}.`);
+
+    // Updated reservation
+    let resz = {
+        "username": currentUser,
+        "laptop": req.params.newLaptop,
+        "start_date": req.params.newDate,
+        "start_time": req.params.newTime,
+        "num_hours": req.params.newHoursMax
+    }
+
+    // Locate users reservation
+    const userIndex = data.resList.findIndex((elem) => elem.username == currentUser);
+
+    if (userIndex != -1) {
+        data.resList[userIndex] = resz;
+        dumpFlatFile(data);
+        res.send("200 OK.");
+
+    } else {
+        res.send("504 - USER DOES NOT HAVE A RESERVATION");
+    }
 
 });
 
 // Delete reservation
 app.delete("/deleteReservation", (req, res) => {
+    console.log(`Received request to delete reservation from ${currentUser}.`);
 
+    // Locate users index
+    const userIndex = data.resList.findIndex((elem) => elem.username == currentUser);
+
+    if (userIndex != -1) {
+        data.resList.splice(userIndex, 1);
+        dumpFlatFile(data);
+        res.send("200 OK.");
+
+    } else {
+        res.send("504 - USER HAS NO RESERVATION TO DELETE");
+    }
 });
 
 // View reservation
 app.get("/viewReservation", (req, res) => {
+    console.log(`Received request to view reservation from ${currentUser}.`);
+
+    // Retreive user's index
+    const userIndex = data.resList.findIndex((elem) => elem.username == currentUser);
+
+    if (userIndex != -1) {
+        res.send(data.resList[userIndex]);
+
+    } else {
+        res.send("504 - USER HAS NO RESERVATION TO VIEW");
+    }
 
 });
 
 // View all reservations
 app.get("/viewAllReservations", (req, res) => {
+    console.log(`Received request to view all reservations from ${currentUser}.`);
 
+    // Sort array by date and start time in descending order
+    data.resList.sort((res1, res2) => {
+        return new Date(res1.start_date) - new Date(res2.start_date);
+    });
+
+    res.send({"data": data.resList});
 });
 
 // Start server
